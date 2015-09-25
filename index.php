46697a1eb27cd5fc51071990ad1ba04c7a9d4a8a
Kaspars <kasparsj@gmail.com>
2015-09-25 09:15:44 +0300

    initialising the repository

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: JSME for WordPress
+Plugin URI: http://github.com/kasparsj/jsme-for-wordpress
+Description: JSME for WordPress
+Version: 1.0
+Author: Kaspars Jaudzems
+Author URI: http://kaspars.co
+Text Domain: jsme-for-wordpress
+*/
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) exit;
